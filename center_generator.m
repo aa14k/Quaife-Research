38da60c19191e584af244668f9a88de53f31d7c3
Alex Ayoub <43243047+aa14k@users.noreply.github.com>
2019-02-22 11:43:28 -0500

    Add files via upload

--- center_generator.m
+++ center_generator.m
@@ -1,28 +1,33 @@
 % This functions generates random centers that are picked such that
 % No shape intersects another shape
 
-function [X,Y] = center_generator(r,N,xl,xr,yb,yt)
-
+function [X,Y] = center_generator(r,N,xl,xr,yb,yt,R)
+R = R-r;
 X = zeros(N,1);
 Y = zeros(N,1); 
 X(1) = xl + (xr-xl)*rand();
 Y(1) = yb + (yt-yb)*rand();
+while X(1)^2 + Y(1)^2 > R^2
+    X(1) = xl + (xr-xl)*rand();
+    Y(1) = yb + (yt-yb)*rand();
+end
 i = 2;
 l = 1;
 while(i<(N+1))
     x = xl + (xr-xl)*rand();
     y = yb + (yt-yb)*rand();
-    distance = zeros(i-1,1);
-    for j=1:i
-        distance(j) = sqrt((x-X(j))^2 + (y-Y(j))^2);
+    if x^2 + y^2 < R^2
+        distance = zeros(i-1,1);
+        for j=1:i
+            distance(j) = sqrt((x-X(j))^2 + (y-Y(j))^2);
+        end
+        disTest = distance < r;
+        if sum(disTest) == 0
+            X(i) = x;
+            Y(i) = y;
+            i = i + 1;
+        end
     end
-    disTest = distance < r;
-    if sum(disTest) == 0
-        X(i) = x;
-        Y(i) = y;
-        i = i + 1;
-    end
-    l = l + 1;
 end
 
 end
