36ff75d51ce55afbf8ef2cc545fbc6a37a276d18
jcs15c <JacobSpainhour782@gmail.com>
2019-04-05 11:15:34 -0400

    Update multi_circle_polar_walker.m

--- multi_circle_polar_walker.m
+++ multi_circle_polar_walker.m
@@ -1,17 +1,30 @@
 % do multiple simulations per initial location
 
-absorbing_radius = 1;
-N = 3;
+% poisonville
+vec1 = @(z) 1 - imag(z).^2;
+
+% extensional
+vec2 = @(z) -real(z) + 1i*imag(z);
+
+% counterclockwise rotation
+vec3 = @(z) 1i*z;
+
+% shear
+vec4 = @(z) imag(z);
+
+absorbing_radius = 1/3;
+N = 1;
 
 %!!!DO NOT MAKE ANY OF THESE NUMBERS LESS THAN THE BOUNDARY RADIUS!!!
-xmin = -3;
-xmax = 3;
-ymin = -3;
-ymax = 3;
+xmin = -1;
+xmax = 1;
+ymin = -1;
+ymax = 1;
 
-bounding_radius = 3; % raduis of the outer(boundary) circle
+bounding_radius = 1; % raduis of the outer(boundary) circle
 [xc, yc] = center_generator(absorbing_radius,N,xmin,xmax,ymin,ymax,bounding_radius);
 centers = xc + 1i*yc;
+centers = [0];
 
 %r = @(theta) absorbing_radius; %our function for our absorbing surface (for plotting purposes)
 r2 = @(theta) bounding_radius; %our function for our boundary circle (for plotting purposes)
@@ -22,17 +35,18 @@ theta = linspace(0,2*pi,1000);
 geoms = zeros(1000, N);
 for i=1:N
     disp( [xc(i) yc(i)] )
-    geoms(:, i) = exp(1i*theta) + centers(i);    
+    geoms(:, i) = absorbing_radius*exp(1i*theta) + centers(i);    
 end
 geom2 = r2(theta).*exp(1i*theta);
 
 
-
-nx = 400; %number of points in the x direction
-ny = 400; %number of points in the y direction
+nx = 100; %number of points in the x direction
+ny = 100; %number of points in the y direction
 
 
 [x,y] = meshgrid(linspace(xmin,xmax,nx),linspace(ymin,ymax,ny));
+
+v = vec4( x + 1i*y );
 
 init_pts = x(:) + 1i*y(:);% initial location of the random walks
 npts = numel(init_pts);% number of initial points
@@ -42,13 +56,13 @@ mfpt = zeros(npts,1);% initialize our mean first passage time vector
  
 D = 1; % diffusion coefficient
  
-dt = 0.1;% our step size for the random walker
+dt = 0.01;% our step size for the random walker
 for k = 1:numel(init_pts)%iterate over each of the nx by ny inital pts
       if abs(init_pts(k)) > bounding_radius %if the initial location of the random walker...
                                             %is not in the boundary circle then...  
           % ...Do nothing
       else    
-      disp(k)
+      disp(init_pts(k))
       z = init_pts(k)*ones(nruns,1); 
       zold = z;
       znew = zold;
@@ -69,21 +83,25 @@ for k = 1:numel(init_pts)%iterate over each of the nx by ny inital pts
         
         fpt(s) = fpt(s) + dt;
         omega = 2*pi*rand(nruns,1);
-        znew(s) = zold(s) + D*dt*exp(1i*omega(s));
+        
+        vold = interp2(x,y,v,real(zold(s)),imag(zold(s)), 'nearest');
+        znew(s) = zold(s) + D*dt*(exp(1i*omega(s)) + vold);
+    
+        %znew(s) = zold(s) + D*dt*(exp(1i*omega(s)) + vec4(zold(s)));
     
         if any(abs(znew) > bounding_radius) % to account for our closed boundary
-          s = find(abs(znew) > bounding_radius);
-          overshoot = abs(znew(s)) - bounding_radius;
-          znew(s) = (bounding_radius - overshoot).*znew(s)./abs(znew(s));
+            s = find(abs(znew) > bounding_radius);    
+            % Simulates the bounding radius acting as an absorbing radius
+            znew(s) = centers(1);
+            % overshoot = abs(znew(s)) - bounding_radius;
+            % znew(s) = (bounding_radius - overshoot).*znew(s)./abs(znew(s));
         end
         % end min_dist
         dist = abs(znew - centers(1));
         for i=2:N
           dist = min( dist, abs(znew - centers(i)));
         end
-        %dist = min(abs(znew-centers(1, 1)), abs(znew-centers(2, 1)), abs(znew-centers(3, 1)));
-        %[inds, dist] = dsearchn(centers, znew);
-        %dist = abs(dist);
+
         zold = znew;
       end
       
